7837a9f66e1625fc8219054bdf4952715a31f2f1
Egon Willighagen <egon.willighagen@gmail.com>
2021-10-16 09:48:13 +0200

    Report any fail, not just the last :/

--- src/java/main/org/wikipathways/covid/CheckRDF.java
+++ src/java/main/org/wikipathways/covid/CheckRDF.java
@@ -104,6 +104,7 @@ public class CheckRDF {
         String currentTestClass = "";
         String currentTest = "";
         boolean currentTestClassHasFails = false;
+        boolean anyTestClassHasFails = false;
         String currentTestClassMessages = "";
         String message = "";
         String errors = "";
@@ -210,10 +211,9 @@ public class CheckRDF {
         }
         if (currentTestClassHasFails) {
           report.println(currentTestClassMessages + message);
-          reportStatus.println("status=⨯");
+          anyTestClassHasFails = true;
         } else {
           report.println(": all " + testClassTests + " tests OK!");
-          reportStatus.println("status=✓");
         }
 
         report.println();
@@ -235,6 +235,12 @@ public class CheckRDF {
                 }
             }
         }
+        if (anyTestClassHasFails) {
+          reportStatus.println("status=⨯");
+        } else {
+          reportStatus.println("status=✓");
+        }
+
         report.flush(); report.close();
         reportStatus.flush(); reportStatus.close();
     }
